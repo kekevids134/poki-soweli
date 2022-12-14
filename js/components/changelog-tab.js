21169a14a8c2fcdb79df562903bb5f56ac83ce99
kekevids134 <aydenlim105116@gmail.com>
2022-12-15 11:07:01 +1300

    v0.0.2.1

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,12 +1,19 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
-    <template v-slot:title>v0.0.2</template>
-    <template v-slot:text>i think i'm done with the settings??
-    </template>
-    <template v-slot:title>v0.0.1</template>
-    <template v-slot:text>doing the settings, like 75% of the way there
-    </template>
+        <template v-slot:title>v0.0.2.1</template>
+        <template v-slot:text>THE TINIEST BUG FIX EVER!!!!!!!!!!
+        </template>
+    </guide-item>
+    <guide-item>
+        <template v-slot:title>v0.0.2</template>
+        <template v-slot:text>i think i'm done with the settings??
+        </template>
+    </guide-item>
+    <guide-item>
+        <template v-slot:title>v0.0.1</template>
+        <template v-slot:text>doing the settings, like 75% of the way there
+        </template>
     </guide-item>
 </div>`
 })
